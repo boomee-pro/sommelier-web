f725b435f3fb515920a0f70dc3b0f050a447231b
Sylvain Bouillon <sylvain.bouillon@live.be>
2022-04-02 21:52:00 +0200

    add section

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,40 @@ import Layout from '../components/Layout/layout'
 import styles from '../styles/home.module.scss'
 import Image from 'next/image';
 
+import Card from '../components/card';
+
 export default function Home() {
+
+  const wines = [
+    {
+      id: 0,
+      name: "Montrachet Grand Cru",
+      price: 4205,
+      origin: "France",
+      domain: "Domaine Leflaive",
+      img: "/wines/grand-cru.png",
+      flag: "/flags/FR.svg"
+    },
+    {
+      id: 1,
+      name: "Roumier Musigny Grand Cru",
+      price: 4205,
+      origin: "France",
+      domain: "Domaine Georges & Christophe",
+      img: "/wines/musigny.png",
+      flag: "/flags/FR.svg"
+    },
+    {
+      id: 2,
+      name: "Barolo DOCG",
+      price: 4205,
+      origin: "Italy",
+      domain: "Bruno Giacosa Collina Rionda",
+      img: "/wines/barolo.png",
+      flag: "/flags/IT.svg"
+    },
+  ]
+
   return (
     <>
       <Head>
@@ -16,6 +49,17 @@ export default function Home() {
 
         <section className={styles.sectionImg}>
           <img className={styles.image} src="/header.png" alt=""/>
+          <h3>Lorem Ipsum dolor sit amet</h3>
+        </section>
+
+        <section className={styles.favoriteWine}>
+          <h3>Vos préférés</h3>
+          <div className={styles.favoriteWineContainer}>
+          {wines.map((wine) => 
+            <Card key={wine.id} wine={wine}/>
+          )}
+          </div>
+          <button>Voir tous les vins</button>
         </section>
 
       </div>
